a72e320a13346e0fae9df25acdf1ef8e84dd3e81
root <root@ip-172-31-93-194.ec2.internal>
2025-08-01 06:42:13 +0000

    This is my first commit

---
+++ file.java
@@ -0,0 +1,19 @@
+// Main.java
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Git!"); 
+        // This is a simple Java program to get started with Git.
+        // Feel free to modify this file and create new ones to experiment with Git commands.
+    }
+
+    public void greet(String name) {
+        System.out.println("Hello, " + name + "!");
+    }
+
+    // Add more methods and classes here to simulate a growing project
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
+
